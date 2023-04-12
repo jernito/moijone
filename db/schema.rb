e73bfb2e2a211ff0fefd669c05ce88abdbf2be80
jernito naisso <jernito.naisso@gmail.com>
2023-04-12 16:01:33 +0200

    apporter un changement sur ma table resarvation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_094807) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_135720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_094807) do
   create_table "reservations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -50,4 +52,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_094807) do
   end
 
   add_foreign_key "recettes", "restaurants"
+  add_foreign_key "reservations", "users"
 end
